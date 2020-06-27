fc4dc5fd79eeebc11d279caa36b275bc78ba6d5b
Raman Thatikunta <raman_k_t@hotmail.com>
2020-06-27 17:07:45 -0400

    Add project files.

---
+++ MoshDbFirst/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoshDbFirst
+{
+
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.Courses;
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Title + ',' + course.Level);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
